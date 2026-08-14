eb51c35017ff0c8e77b3bb7d53ba35dfbc565525
arefseifipro <atilesr28@gmail.com>
2026-08-14 22:38:20 +0330

    Delete config.js

--- test-porgress/js/config.js
+++
@@ -1,8 +0,0 @@
-const GITHUB_CONFIG = {
-    owner: "arefseifipro",        
-    repo: "arfito", 
-    token: "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", 
-    filePath: "data.json"
-};
-
-const ADMIN_CODE = "54667";
